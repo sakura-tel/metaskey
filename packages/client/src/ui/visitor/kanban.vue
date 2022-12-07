2d40c2ecfc9a3188243b789bec632a8a1ffbe102
atsu1125 <atsuchan@atsuchan.page>
2022-12-07 18:20:48 +0900

    Metaskey

--- packages/client/src/ui/visitor/kanban.vue
+++ packages/client/src/ui/visitor/kanban.vue
@@ -28,7 +28,7 @@
 				</div>
 				<div v-if="poweredBy" class="powered-by">
 					<b><MkA to="/">{{ host }}</MkA></b>
-					<small>Powered by <a href="https://github.com/misskey-dev/misskey" target="_blank">Misskey</a></small>
+					<small>Powered by <a href="https://github.com/sakura-tel/metaskey" target="_blank">Misskey</a></small>
 				</div>
 			</template>
 		</div>
