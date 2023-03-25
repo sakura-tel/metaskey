630c7a90c7701debb47b8691daea2108bd7b28fc
atsu1125 <atsuchan@atsuchan.page>
2023-03-25 12:27:01 +0900

    Fix: Change: Only moderators can show blocked user's post

--- packages/backend/src/server/api/endpoints/notes/renotes.ts
+++ packages/backend/src/server/api/endpoints/notes/renotes.ts
@@ -70,7 +70,7 @@ export default define(meta, async (ps, user) => {
 	generateVisibilityQuery(query, user);
 	if (user) generateMutedUserQuery(query, user);
 	if (user && !user.isAdmin && !user.isModerator) {
-		generateBlockedUserQuery(query, me);
+		generateBlockedUserQuery(query, user);
 	}
 
 	const renotes = await query.take(ps.limit!).getMany();
